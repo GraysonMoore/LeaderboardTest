c69b95be0cd711a0ae539e5f2d25477f7008e1cb
GraysonMoore <moor8456@hotmail.com>
2023-05-13 14:23:29 -0700

    Update scriptdos.js

--- scriptdos.js
+++ scriptdos.js
@@ -10,16 +10,17 @@ function load() {
       if(!username) {
         return alert("Enter Your Name");
       }
+      const path = "https://freeriderjumps-default-rtdb.firebaseio.com/";
       const method = "POST";
       const headers = {"Content-Type": "application/json"}
       const body = JSON.stringify({ username });
       const redirect = "error";
       try {
-        await fetch({ method, headers, body, redirect });
-        playername = username;
-        localStorage.setItem("playername", playername);
-        location.href = "leaderboard.html";
-      } catch(ex) {
-        alert("That name is already being used");
+        await fetch(path, { method, headers, body, redirect });
+        //playername = username;
+        //localStorage.setItem("playername", playername);
+        //location.href = "leaderboard.html";
+      //} catch(ex) {
+        //alert("That name is already being used");
       }
     };
