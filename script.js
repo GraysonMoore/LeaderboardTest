3a84caeca2e2ff79ef08bcffeef78cd1c8572787
GraysonMoore <moor8456@hotmail.com>
2023-05-13 11:30:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -22,29 +22,7 @@ var r=this.add.container(0,290);r.add(n),r.add(s),r.alpha=0,this.add.tween({targ
 //My Code -Grayson
 
 
-function load() {
-  const name = localStorage.getItem("playername");
-  document.getElementById("name").value = name;
-};
 
-async function createAccount() {
-  const username = document.getElementById("name").value;
-  if(!username) {
-    return alert("Enter Your Name");
-  }
-  const method = "POST";
-  const headers = {"Content-Type": "application/json"}
-  const body = JSON.stringify({ username });
-  const redirect = "error";
-  try {
-    await fetch({ method, headers, body, redirect });
-    playername = username;
-    localStorage.setItem("playername", playername);
-    location.href = "leaderboard.html";
-  } catch(ex) {
-    alert("That name is already being used");
-  }
-};
 
 async function login() {
   const username = document.getElementById("name").value;
