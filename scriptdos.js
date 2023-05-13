f2317b628ae0ef24a5b549a4c1f8b1fdacb1f696
GraysonMoore <moor8456@hotmail.com>
2023-05-13 12:51:11 -0700

    Update scriptdos.js

--- scriptdos.js
+++ scriptdos.js
@@ -1,1 +1,23 @@
+function load() {
+      const name = localStorage.getItem("playername");
+      document.getElementById("name").value = name;
+    };
 
+    async function createAccount() {
+      const username = document.getElementById("name").value;
+      if(!username) {
+        return alert("Enter Your Name");
+      }
+      const method = "POST";
+      const headers = {"Content-Type": "application/json"}
+      const body = JSON.stringify({ username });
+      const redirect = "error";
+      try {
+        await fetch({ method, headers, body, redirect });
+        playername = username;
+        localStorage.setItem("playername", playername);
+        location.href = "leaderboard.html";
+      } catch(ex) {
+        alert("That name is already being used");
+      }
+    };
